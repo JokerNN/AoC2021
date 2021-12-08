404a5278cd0d3f1298f61d09708bd8b950449d96
Andrey Taktaev <andrey.taktaev@booking.com>
2021-12-08 12:31:07 +0100

    day7

---
+++ 7.py
@@ -0,0 +1,37 @@
+from utils import get_input_lines
+import math
+import statistics
+from functools import lru_cache
+
+input = [int(n) for n in get_input_lines('7.txt')[0].split(',')]
+# input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+median = statistics.median_low(input)
+s = 0
+for crab in input:
+    s += abs(median - crab)
+
+print(f'Answer 1: {s}')
+
+
+@lru_cache(None)
+def arithmetic_sum(n):
+    a1 = 1
+    an = a1 + (n - 1)
+
+    s = n * (a1 + an) / 2
+    return int(s)
+
+
+mean = round(statistics.mean(input))
+max_crab = max(input)
+
+ms = float('inf')
+for test_crab in range(max_crab + 1):
+    s = 0
+    for crab in input:
+        s += arithmetic_sum(abs(test_crab - crab))
+
+    ms = min(s, ms)
+
+print(f'Answer 2: {ms}')
